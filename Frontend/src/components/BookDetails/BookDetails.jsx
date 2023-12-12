4440fe5359dc0a3bdd5256d1c21175830cd8955c
Soodenn <ANH.NT13147@sinhvien.hoasen.edu.vn>
2023-12-12 23:33:59 +0700

    a

--- Frontend/src/components/BookDetails/BookDetails.jsx
+++ Frontend/src/components/BookDetails/BookDetails.jsx
@@ -1,67 +1,49 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Loading from "../Loader/Loader";
-import coverImg from "../../images/cover_not_found.jpg";
+// import Loading from "../Loader/Loader";
+// import coverImg from "../../images/cover_not_found.jpg";
 import "./BookDetails.css";
-import {FaArrowLeft} from "react-icons/fa";
+import book1 from "../../images/book1.png";
+import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
-
-const URL = "https://openlibrary.org/works/";
+const URL = "http://localhost:8080/books/id";
 
 const BookDetails = () => {
-  const {id} = useParams();
-  const [loading, setLoading] = useState(false);
-  const [book, setBook] = useState(null);
+  const { id } = useParams();
+  const [book, setBooks] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    setLoading(true);
-    async function getBookDetails(){
-      try{
-        const response = await fetch(`${URL}${id}.json`);
+    // Gọi hàm get từ API ở đây và cập nhật state books
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${URL}/${id}`); // Thay đổi URL API thực tế của bạn
         const data = await response.json();
-        console.log(data);
-
-        if(data){
-          const {description, title, covers, subject_places, subject_times, subjects} = data;
-          const newBook = {
-            description: description ? description.value : "No description found",
-            title: title,
-            cover_img: covers ? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg` : coverImg,
-            subject_places: subject_places ? subject_places.join(", ") : "No subject places found",
-            subject_times : subject_times ? subject_times.join(", ") : "No subject times found",
-            subjects: subjects ? subjects.join(", ") : "No subjects found"
-          };
-          setBook(newBook);
-        } else {
-          setBook(null);
-        }
-        setLoading(false);
-      } catch(error){
-        console.log(error);
-        setLoading(false);
+        setBooks(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-    }
-    getBookDetails();
-  }, [id]);
+    };
 
-  if(loading) return <Loading />;
+    fetchData();
+  }, [id]);
 
   return (
     <section className='book-details'>
       <div className='container'>
         <button type='button' className='flex flex-c back-btn' onClick={() => navigate("/book")}>
-          <FaArrowLeft size = {22} />
+          <FaArrowLeft size={22} />
           <span className='fs-18 fw-6'>Go Back</span>
         </button>
 
         <div className='book-details-content grid'>
           <div className='book-details-img'>
-            <img src = {book?.cover_img} alt = "cover img" />
+            {/* <img src={`../../images/${book?.BookImage}`} alt="cover img" /> */}
+            <img src={book1} alt="cover img" />
           </div>
           <div className='book-details-info'>
             <div className='book-details-item title'>
-              <span className='fw-6 fs-24'>{book?.title}</span>
+              <span className='fw-6 fs-24'>{book?.BookName}</span>
             </div>
             <div className='book-details-item description'>
               <span>{book?.description}</span>
@@ -84,5 +66,6 @@ const BookDetails = () => {
     </section>
   )
 }
+
 
 export default BookDetails
--- Frontend/src/components/SearchForm/SearchForm.jsx
+++ Frontend/src/components/SearchForm/SearchForm.jsx
@@ -1,27 +1,47 @@
-import React, {useRef, useEffect} from 'react';
-import {FaSearch} from "react-icons/fa";
+import React, { useRef, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { useGlobalContext } from '../../context.';
-import "./SearchForm.css";
+import './SearchForm.css';
 
 const SearchForm = () => {
-  const {setSearchTerm, setResultTitle} = useGlobalContext();
+  const { setSearchTerm, setResultTitle } = useGlobalContext();
   const searchText = useRef('');
   const navigate = useNavigate();
 
   useEffect(() => searchText.current.focus(), []);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let tempSearchTerm = searchText.current.value.trim();
-    if((tempSearchTerm.replace(/[^\w\s]/gi,"")).length === 0){
-      setSearchTerm("the lost world");
-      setResultTitle("Please Enter Something ...");
-    } else {
-      setSearchTerm(searchText.current.value);
-    }
 
-    navigate("/book");
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Ngăn chặn hành vi mặc định của sự kiện submit form
+
+    let tempSearchTerm = searchText.current.value.trim();
+
+    if ((tempSearchTerm.replace(/[^\w\s]/gi, '')).length === 0) {
+      setSearchTerm('the lost world');
+      setResultTitle('Please Enter Something ...');
+    } else {
+      try {
+        // Gọi API để lấy dữ liệu từ server
+        const response = await fetch(`http://localhost:8080/books/name/${tempSearchTerm}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        // Xử lý dữ liệu nhận được từ server (data)
+        console.log(data);
+
+        setSearchTerm(tempSearchTerm);
+        // Chuyển hướng sau khi setSearchTerm đã được thực hiện
+        navigate('/book');
+      } catch (error) {
+        console.error('Error fetching data from server:', error);
+        // Xử lý lỗi nếu cần thiết
+      }
+    }
   };
+
+
 
   return (
     <div className='search-form'>
@@ -29,16 +49,16 @@ const SearchForm = () => {
         <div className='search-form-content'>
           <form className='search-form' onSubmit={handleSubmit}>
             <div className='search-form-elem flex flex-sb bg-white'>
-              <input type = "text" className='form-control' placeholder='The Lost World ...' ref = {searchText} />
-              <button type = "submit" className='flex flex-c' onClick={handleSubmit}>
-                <FaSearch className='text-purple' size = {32} />
+              <input type='text' className='form-control' placeholder='The Lost World ...' ref={searchText} />
+              <button type='submit' className='flex flex-c' onClick={handleSubmit}>
+                <FaSearch className='text-purple' size={32} />
               </button>
             </div>
           </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
